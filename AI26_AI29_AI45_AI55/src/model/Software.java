8b62b3be64dc9b2b2ed1ebff273263176925c755
rankovic1011 <105729637+rankovic1011@users.noreply.github.com>
2022-06-10 18:56:04 +0200

    Klasama Software.java i WorkPlace.java dodati atributi

--- AI26_AI29_AI45_AI55/src/model/Software.java
+++ AI26_AI29_AI45_AI55/src/model/Software.java
@@ -1,5 +1,68 @@
 package model;
 
-public class Software {
+import java.util.ArrayList;
 
+public class Software {
+	
+	private String name;
+	private ArrayList<Brush> brushes =  new ArrayList<>();
+	private FileFormat fileFormat;
+	private Render render;
+	private ArrayList<String> animationTools =  new ArrayList<>(); // maybe change String to Object (make AnimationTool class)
+	
+	public Software() {
+		
+	}
+
+	public Software(String name, ArrayList<Brush> brushes, FileFormat fileFormat, Render render,
+			ArrayList<String> animationTools) {
+		super();
+		this.name = name;
+		this.brushes = brushes;
+		this.fileFormat = fileFormat;
+		this.render = render;
+		this.animationTools = animationTools;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Brush> getBrushes() {
+		return brushes;
+	}
+
+	public void setBrushes(ArrayList<Brush> brushes) {
+		this.brushes = brushes;
+	}
+
+	public FileFormat getFileFormat() {
+		return fileFormat;
+	}
+
+	public void setFileFormat(FileFormat fileFormat) {
+		this.fileFormat = fileFormat;
+	}
+
+	public Render getRender() {
+		return render;
+	}
+
+	public void setRender(Render render) {
+		this.render = render;
+	}
+
+	public ArrayList<String> getAnimationTools() {
+		return animationTools;
+	}
+
+	public void setAnimationTools(ArrayList<String> animationTools) {
+		this.animationTools = animationTools;
+	}
+	
+	
 }
