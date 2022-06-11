239f0725733778c6fab3c06574aad503f89d9420
GalaMiljevic <105778853+GalaMiljevic@users.noreply.github.com>
2022-06-11 11:00:26 +0200

    Klasama Adress.java i Brush.java dodati atributi

--- AI26_AI29_AI45_AI55/src/model/Adress.java
+++ AI26_AI29_AI45_AI55/src/model/Adress.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Adress {
+	
+	private String street;
+	private String number; // String not int because adress number can be 11A 11B ...
+	private String city;
+	
+    public Adress() {
+		
+	}
+
+	public Adress(String street, String number, String city) {
+		super();
+		this.street = street;
+		this.number = number;
+		this.city = city;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() {
+		return street + " " + number + " " + city ;
+	}
 
 }
--- AI26_AI29_AI45_AI55/src/model/Brush.java
+++ AI26_AI29_AI45_AI55/src/model/Brush.java
@@ -1,5 +1,44 @@
 package model;
 
 public class Brush {
+	
+	private String name;
+	private String purpose;
+	private Color color;
+	
+    public Brush() {
+		
+	}
+    
+    public Brush(String name, String purpose, Color color) {
+		super();
+		this.name = name;
+		this.purpose = purpose;
+		this.color = color;
+	}
+    
+    public String getName() {
+		return name;
+	}
+    
+    public void setName(String name) {
+		this.name = name;
+	}
+    
+    public String getPurpose() {
+		return purpose;
+	}
+    
+    public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+    
+    public Color getColor() {
+		return color;
+	}
+    
+    public void setColor(Color color) {
+		this.color = color;
+	}
 
 }
