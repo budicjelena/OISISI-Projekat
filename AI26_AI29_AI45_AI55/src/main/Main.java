06d4c1aa66bd1d30b4ecff3800c09a8ec0264725
GalaMiljevic <105778853+GalaMiljevic@users.noreply.github.com>
2022-06-15 15:23:12 +0200

    Izmena u Main.java klasi

--- AI26_AI29_AI45_AI55/src/main/Main.java
+++ AI26_AI29_AI45_AI55/src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import gui.MainFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		MainFrame.getInstance();
 	}
 }
