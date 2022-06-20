746634f3afe8e10c99d181111165446653a9dd54
jagodic45 <jagodicd99@gmail.com>
2022-06-20 16:17:29 +0200

    Kreirane klase RenderData.java u okviru data paketa i RenderTab.java u okviru gui paketa

---
+++ AI26_AI29_AI45_AI55/src/data/RenderData.java
@@ -0,0 +1,62 @@
+package data;
+
+import java.util.ArrayList;
+
+import model.Camera;
+import model.Material;
+import model.Render;
+
+public class RenderData {
+
+	private ArrayList<Render> renders = new ArrayList<>();
+	
+	public RenderData() {
+		loadDummyData();
+	}
+	
+	private void loadDummyData() {
+		ArrayList<Material> m1 = new ArrayList<>();
+		ArrayList<Material> m2 = new ArrayList<>();
+		m1.add(Material.MAT1);
+		m1.add(Material.MAT2);
+		m2.add(Material.MAT2);
+		m2.add(Material.MAT3);
+		
+		ArrayList<model.Object> o1 = new ArrayList<>();
+		ArrayList<model.Object> o2 = new ArrayList<>();
+		o1.add(model.Object.OBJ_1);
+		o1.add(model.Object.OBJ_2);
+		o1.add(model.Object.OBJ_3);
+		
+		ArrayList<Camera> c1 = new ArrayList<>();
+		ArrayList<Camera> c2 = new ArrayList<>();
+		
+		c1.add(Camera.HD);
+		c1.add(Camera.ROUNDED);
+		
+		c2.add(Camera.HD);
+		c2.add(Camera.ULTRA_HD);
+		c2.add(Camera.WIDE);
+		
+		Render r1= new Render("r1", m1, c1, o1);
+		Render r2= new Render("r2", m2, c2, o2);
+		Render r3= new Render("r3", m1, c2, o2);
+		
+		renders.add(r1);
+		renders.add(r2);
+		renders.add(r3);
+	}
+	
+	public ArrayList<Render> getRenders() {
+		return renders;
+	}
+	
+	public void setRenders(ArrayList<Render> renders) {
+		this.renders = renders;
+	}
+	
+	public void removeRenderFromList(int index) {
+		//TODO Auto-generated method stub
+		this.renders.remove(index);
+	}
+}
---
+++ AI26_AI29_AI45_AI55/src/gui/RenderTab.java
@@ -0,0 +1,215 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import data.BrushData;
+import data.EmployeeData;
+import data.RenderData;
+import model.Brush;
+import model.Camera;
+import model.Employee;
+import model.Material;
+import model.Render;
+import model.Software;
+
+public class RenderTab extends JSplitPane {
+
+	private RenderData renderData;
+	private JTable jTable;
+	private DefaultTableModel tableModel;
+	private JPanel tablePanel;
+	private JPanel infoPanel;
+
+	public RenderTab(RenderData renderData) {
+		this.renderData = renderData;
+
+		this.tablePanel = new JPanel(new FlowLayout());
+		this.infoPanel = new JPanel(new FlowLayout());
+
+		this.setLeftComponent(tablePanel);
+		this.setRightComponent(infoPanel);
+		this.setOneTouchExpandable(false);
+		this.setResizeWeight(0.25);// play with values to adjust split rate of left and right pannel
+
+		fillEmployeeTable();
+	}
+
+	public void fillEmployeeTable() {
+		String[] header = new String[] { "Name" };
+		Object[][] renders = new Object[this.renderData.getRenders().size()][header.length];
+
+		for (int i = 0; i < this.renderData.getRenders().size(); i++) {
+
+			Render render = this.renderData.getRenders().get(i);
+			renders[i][0] = render.getName();
+
+		}
+
+		jTable = new JTable();
+		tableModel = (DefaultTableModel) jTable.getModel();
+		tableModel.setDataVector(renders, header);
+		jTable.setRowSelectionAllowed(true);
+		jTable.setColumnSelectionAllowed(false);
+		jTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jTable.setDefaultEditor(Object.class, null);
+		JScrollPane scrollPane = new JScrollPane(jTable);
+		this.tablePanel.add(scrollPane);
+
+		jTable.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 1) {
+					JTable target = (JTable) e.getSource();
+					int row = target.getSelectedRow();
+					int column = target.getSelectedColumn();
+					infoPanel.removeAll();
+					fillInfoPanel(renderData.getRenders().get(row));
+				}
+			}
+		});
+	}
+
+	private void fillInfoPanel(Render  render) {
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(1, 2));
+
+		JLabel name;
+		JTextField tname;
+
+		name = new JLabel("NAME");
+		tname = new JTextField(20);
+		tname.setEditable(false);
+		tname.setText(render.getName());
+
+
+
+		p.add(name);
+		p.add(tname);
+
+		JScrollPane spCameras = fillCamerasTable(render.getCameras());
+		JScrollPane spMaterials = fillMaterialsTable(render.getMaterials());
+		JScrollPane spObjects = fillObjectsTable(render.getObjects());
+
+
+		this.infoPanel.add(p);
+		this.infoPanel.add(spCameras);
+		this.infoPanel.add(spMaterials);
+		this.infoPanel.add(spObjects);
+
+		this.setSize(400, 540);
+	}
+
+	private JScrollPane fillMaterialsTable(ArrayList<Material> materials) {
+		JTable jTable = new JTable();
+		String[] header = new String[] { "MATERIALS" };
+		Object[][] employees = new Object[materials.size()][header.length];
+
+		for (int i = 0; i < materials.size(); i++) {
+			employees[i][0] = materials.get(i).toString();
+		}
+
+		DefaultTableModel tableModelSoftware;
+
+		tableModelSoftware = (DefaultTableModel) jTable.getModel();
+		tableModelSoftware.setDataVector(employees, header);
+		jTable.setColumnSelectionAllowed(false);
+		jTable.setRowSelectionAllowed(false);
+		jTable.setDefaultEditor(Object.class, null);
+		jTable.setPreferredScrollableViewportSize(new Dimension(400, 50));
+		JScrollPane scrollPane = new JScrollPane(jTable);
+		return scrollPane;
+
+	}
+	
+	private JScrollPane fillObjectsTable(ArrayList<model.Object> objects) {
+		JTable jTable = new JTable();
+		String[] header = new String[] { "OBJECTS" };
+		Object[][] employees = new Object[objects.size()][header.length];
+
+		for (int i = 0; i < objects.size(); i++) {
+			employees[i][0] = objects.get(i).toString();
+		}
+
+		DefaultTableModel tableModelSoftware;
+
+		tableModelSoftware = (DefaultTableModel) jTable.getModel();
+		tableModelSoftware.setDataVector(employees, header);
+		jTable.setColumnSelectionAllowed(false);
+		jTable.setRowSelectionAllowed(false);
+		jTable.setDefaultEditor(Object.class, null);
+		jTable.setPreferredScrollableViewportSize(new Dimension(400, 50));
+
+		JScrollPane scrollPane = new JScrollPane(jTable);
+		return scrollPane;
+
+	}
+	
+	private JScrollPane fillCamerasTable(ArrayList<Camera> cameras) {
+		JTable jTable = new JTable();
+		String[] header = new String[] { "CAMERAS" };
+		Object[][] employees = new Object[cameras.size()][header.length];
+
+		for (int i = 0; i < cameras.size(); i++) {
+			employees[i][0] = cameras.get(i).toString();
+		}
+
+		DefaultTableModel tableModelSoftware;
+
+		tableModelSoftware = (DefaultTableModel) jTable.getModel();
+		tableModelSoftware.setDataVector(employees, header);
+		jTable.setColumnSelectionAllowed(false);
+		jTable.setRowSelectionAllowed(false);
+		jTable.setDefaultEditor(Object.class, null);
+		jTable.setPreferredScrollableViewportSize(new Dimension(400, 50));
+
+		JScrollPane scrollPane = new JScrollPane(jTable);
+		return scrollPane;
+
+	}
+	
+	
+	public void deleteRender() {
+		if (jTable.getSelectedRow() >= 0) { // Checking if row is selected
+			int choice = JOptionPane.showConfirmDialog(null, "Do you want to proceed?", "Warning.",
+					JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);// 0 =yes , 1 =no
+
+			if (choice == 0) {
+				this.renderData.removeRenderFromList(jTable.getSelectedRow()); // pass selected row index that
+																				// corresponds to index of list
+				tableModel.removeRow(jTable.getSelectedRow());// remove row from table
+				this.infoPanel.removeAll(); // remove info panel of deleted software
+				this.repaint(); // need this to make visual change
+			}
+		} else {
+			// show error message if no row is selected
+			JOptionPane.showMessageDialog(null, "You need to select row!", "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	public DefaultTableModel getTableModel() {
+		return this.tableModel;
+	}
+
+	public int getSelectedEmployeeIndex() {
+		return this.jTable.getSelectedRow();
+	}
+
+	public JTable getTable() {
+		return this.jTable;
+	}
+}
