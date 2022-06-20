ed7b768472c656d49adf2b5bec111bbbadc0eb29
Jelena <jelenabudic123@gmail.com>
2022-06-20 15:19:22 +0200

    Kreirane klase BrushData.java u okviru packeta data i BrushTab.java u okviru packeta gui

---
+++ AI26_AI29_AI45_AI55/src/data/BrushData.java
@@ -0,0 +1,45 @@
+package data;
+
+import java.util.ArrayList;
+
+import model.Brush;
+import model.Color;
+
+public class BrushData {
+	
+	private ArrayList<Brush> brushes = new ArrayList<>();
+	
+	public BrushData() {
+		loadDummyData();
+	}
+	
+	private void loadDummyData() {
+		
+		Brush b1 = new Brush("thin", "used for drawing lines", Color.RED);
+		Brush b2 = new Brush("wide", "used for coloring", Color.RED);
+		Brush b3 = new Brush("transparent", "used for shading", Color.YELLOW);
+		Brush b4 = new Brush("ultra wide", "used for coloring background", Color.WHITE);
+		Brush b5 = new Brush("rounded", "used for rounding objects", Color.GREEN);
+		
+		brushes.add(b1);
+		brushes.add(b2);
+		brushes.add(b3);
+		brushes.add(b4);
+		brushes.add(b5);
+		
+	}
+	
+	public ArrayList<Brush> getBrushes() {
+		return brushes;
+	}
+	
+	public void setBrushes(ArrayList<Brush> brushes) {
+		this.brushes = brushes;
+	}
+	
+	public void removeBrushFromList(int index) {
+		//TODO Auto-generated method stub
+		this.brushes.remove(index);
+	}
+
+}
---
+++ AI26_AI29_AI45_AI55/src/gui/BrushTab.java
@@ -0,0 +1,150 @@
+package gui;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import data.BrushData;
+import data.EmployeeData;
+import model.Brush;
+import model.Employee;
+import model.Software;
+
+public class BrushTab extends JSplitPane {
+	
+	private BrushData brushData;
+	private JTable jTable;
+	private DefaultTableModel tableModel;
+	private JPanel tablePanel;
+	private JPanel infoPanel;
+	
+	public BrushTab(BrushData brushData) {
+		this.brushData = brushData;
+		
+		this.tablePanel = new JPanel(new FlowLayout());
+		this.infoPanel = new JPanel(new FlowLayout());
+		
+		this.setLeftComponent(tablePanel);
+		this.setRightComponent(infoPanel);
+		this.setOneTouchExpandable(false);
+		this.setResizeWeight(0.25);
+		
+		fillEmployeeTable();
+	}
+	
+	public void fillEmployeeTable() {
+		String[] header = new String[] {"Name", "Purpose"};
+		Object[][] brushes = new Object[this.brushData.getBrushes().size()][header.length];
+		
+		for(int i = 0; i < this.brushData.getBrushes().size(); i++) {
+			
+			Brush brush = this.brushData.getBrushes().get(i);
+			brushes[i][0] = brush.getName();
+			brushes[i][1] = brush.getPurpose();
+			
+		}
+		
+		jTable = new JTable();
+		tableModel = (DefaultTableModel) jTable.getModel();
+		tableModel.setDataVector(brushes, header);
+		jTable.setRowSelectionAllowed(true);
+		jTable.setColumnSelectionAllowed(false);
+		jTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		jTable.setDefaultEditor(Object.class, null);
+		JScrollPane scrollPane = new JScrollPane(jTable);
+		this.tablePanel.add(scrollPane);
+		
+		jTable.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 1) {
+					JTable target = (JTable) e.getSource();
+					int row = target.getSelectedRow();
+					int column = target.getSelectedColumn();
+					infoPanel.removeAll();
+					fillInfoPanel(brushData.getBrushes().get(row));
+				}
+			}
+		});
+		
+	}
+	
+	private void fillInfoPanel(Brush brush) {
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(3,2));
+		
+		JLabel name, purpose, color;
+		JTextField tname, tpurpose, tcolor;
+		
+		name = new JLabel("NAME");
+		tname = new JTextField(20);
+		tname.setEditable(false);
+		tname.setText(brush.getName());
+		
+		purpose = new JLabel("PURPOSE");
+		tpurpose = new JTextField(20);
+		tpurpose.setEditable(false);
+		tpurpose.setText(brush.getPurpose());
+		
+		color = new JLabel("COLOR");
+		tcolor = new JTextField(20);
+		tcolor.setEditable(false);
+		tcolor.setText("");
+		tcolor.setBackground(Brush.convertColor(brush.getColor()));
+		
+		p.add(name);
+		p.add(tname);
+		
+		p.add(purpose);
+		p.add(tpurpose);
+		
+		p.add(color);
+		p.add(tcolor);
+		
+		this.infoPanel.add(p);
+		this.setSize(400, 180);
+		
+	}
+	
+	public void deleteBrush() {
+		if(jTable.getSelectedRow() >= 0) { //checking if row is selected
+			int choice = JOptionPane.showConfirmDialog(null, "Do you want to proceed?", "Warning.", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE); // 0 = yes 1 = no
+			
+			if(choice == 0) {
+				this.brushData.removeBrushFromList(jTable.getSelectedRow()); //pass selected row index that
+				
+				tableModel.removeRow(jTable.getSelectedRow()); //remove row from table
+				this.infoPanel.removeAll(); //remove info panel of deleted software
+				this.repaint();//need this to make visual change
+				
+			}
+		} else {
+			//show error message if no row is selected
+			JOptionPane.showMessageDialog(null, "You need to select a row!", "ERROR", JOptionPane.ERROR_MESSAGE);
+		} 
+	}
+	
+	public DefaultTableModel getTableModel() {
+		return this.tableModel;
+	}
+	
+	public int getSelectedEmployeeIndex() {
+		return this.jTable.getSelectedRow();
+	}
+
+	public JTable getTable() {
+		return this.jTable;
+	}
+}
